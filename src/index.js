02bf81d5e76c2ef5c2861e15f43b2db9b5a75c79
va0z <va0z@ya.ru>
2019-09-24 21:21:11 +1000

    init: iiiiiihaaaaaa =)

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
-module.exports = function check(str, bracketsConfig) {
+  module.exports = function check(str, bracketsConfig) {
+
   // your solution
+  // console.log(str);
+  arrBrackets = [];
+  for (j=0; j<bracketsConfig.length; ++j ){
+    arrBrackets.push ( bracketsConfig[j].join('') );
+    // console.log(arrBrackets[j]);
+  } 
+
+  // console.log( str );
+  // console.log( arrBrackets.join() );
+
+  let flag=1;
+  var fl1=0;
+  while (flag > 0) {
+    flag=0;
+    for ( i=0; i<arrBrackets.length; ++i) {
+      fl1  = str.indexOf(arrBrackets[i]);
+      if ( flag == 0 && fl1 > -1 ) flag = 1;
+      str = str.replace(arrBrackets[i], "");
+      // console.log("srt = " + str + " : flag = " + flag);
+    }
+  }
+  // console.log(str);
+  if (str.length > 0) return false;
+  else return true ;
 }
